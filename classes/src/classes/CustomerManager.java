d21df60c03dd12465a8f5362803541f02392c421
BATUN IDIKURT <batunidikurt49@gmail.com>
2022-09-27 03:55:29 +0300

    first commit

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class CustomerManager
+{
+	public void add() {
+		System.out.println("Müşteri eklendi");
+	}
+	
+	public void update() {
+		System.out.println("Müşteri güncellendi");
+	}
+	
+	
+	public void remove() {
+		System.out.println("Müşteri silindi");
+	}
+}
