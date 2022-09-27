78f0258aefe708fc8977868544eea6a7fbcdc1dd
BATUN IDIKURT <batunidikurt49@gmail.com>
2022-09-28 02:38:46 +0300

    first commit

--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -4,7 +4,11 @@ public class ProductManager
 {
 	public void add(Product product)
 	{
-		System.out.println(product.name+" isimli ürün eklendi.");
+		System.out.println(product.getName()+" isimli ürün eklendi.");
 	}
 	
+	public void add2(int id,String name,String description,int stockAmount,double price,String renk)
+	{
+		System.out.println("Ürün eklendi.");
+	}
 }
