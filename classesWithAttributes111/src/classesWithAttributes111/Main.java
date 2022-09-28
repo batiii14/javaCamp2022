29a2213e12aa477eb007ac1f9f3b65f3aafefcdb
BATUN IDIKURT <batunidikurt49@gmail.com>
2022-09-28 03:03:38 +0300

    sec

--- classesWithAttributes111/src/classesWithAttributes111/Main.java
+++ classesWithAttributes111/src/classesWithAttributes111/Main.java
@@ -5,20 +5,15 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		Product product = new Product();
-		product.setName("Laptop");
-		product.setId(1);
-		product.setDescription("Asus Laptop");
-		product.setPrice(5000);
-		product.setStockAmount(3);
+		
 		
 		Product product1 = new Product(1,"Laptop","Asus Laptop",3000,2,"Siyah");
 		
 
 		ProductManager productManager = new ProductManager();
-		productManager.add(product);
+		productManager.add(product1);
 		
-		System.out.println(product.getKod());
+		System.out.println(product1.getKod());
 
 		
 		
