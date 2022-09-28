0215c4bf62b3eb52c1c214f26cb810f8329efaf4
BATUN IDIKURT <batunidikurt49@gmail.com>
2022-09-28 03:42:40 +0300

    first commit

---
+++ inheritanceDemo/src/inheritanceDemo/BaseKrediManager.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+public class BaseKrediManager
+{
+	public void hesapla()
+	{
+		System.out.println("Kredi Hesaplandı");
+	}
+
+}
---
+++ inheritanceDemo/src/inheritanceDemo/KrediUI.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+public class KrediUI 
+{	
+	public void krediHesapla(BaseKrediManager baseKrediManager) {
+		baseKrediManager.hesapla();
+		
+	}
+
+}
