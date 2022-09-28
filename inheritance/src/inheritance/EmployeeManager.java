35c1649c55e5dc5c008e9fea95a9e562f7875376
BATUN IDIKURT <batunidikurt49@gmail.com>
2022-09-28 03:26:34 +0300

    first commit

---
+++ inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class EmployeeManager extends PersonManager
+{
+
+	public void bestEmployee()
+	{
+		System.out.println("Ayın elemanı getirildi");
+	}
+
+}
---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class PersonManager
+{
+	public void List()
+	{
+		System.out.println("Müşteri Listelendi");
+	}
+	
+	public void add()
+	{
+		System.out.println("Eklendi");
+	}
+
+}
